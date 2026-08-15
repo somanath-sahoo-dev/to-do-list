c835687b3ea34ec2406428a5f9f0bcd142c5f3a1
Somanath Sahoo <somanath.sahoo.tech@gmail.com>
2026-08-16 00:18:39 +0530

    Intial Commit

---
+++ toDoList.js
@@ -0,0 +1,46 @@
+// let ul = document.querySelector("ul");
+let task = document.querySelector("#task");
+let add = document.querySelector("#add");
+let div = document.querySelector(".div");
+
+add.addEventListener("click", function() {
+    //New Task
+    let newTask = task.value;
+    if(newTask == "") {
+        alert("Please enter a task");
+        return;
+    }
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("div");
+    let newInp = document.createElement("input");
+    newInp.type = "checkbox";
+    newInp.id = newTask;
+    let newLabel = document.createElement("label");
+    newLabel.htmlFor = newTask;
+    newLabel.innerText = newTask;
+    //New Delete Button
+    let newDel = document.createElement("button");
+    newDel.innerText = "Delete";
+    newDel.classList.add("delete");
+
+    newDel.addEventListener("click", function() {
+        newDiv.remove();
+    })
+
+    newDiv.appendChild(newInp);
+    newDiv.appendChild(newLabel);
+    newDiv.appendChild(newDel);
+
+    newInp.addEventListener("change", function() {
+        newDiv.remove();
+    });
+
+    document.body.appendChild(newDiv);
+
+    task.value = "";
+    
+})
+
+function taskCheck() {
+
+}
